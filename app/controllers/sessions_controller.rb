beebbfa926dc69994f40ea06df2944772f954f36
kjdowns <kevin.downs04@gmail.com>
2020-11-30 19:25:55 -0500

    change database to sqlte3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
         
         ##authenticate user credentials
         if !!@user && @user.authenticate(params[:password])
-            message = "It worked!"
-            redirect_to login_path, notice: message
+            #set session and redirect on success
+            session[:user_id] = @user.id
+            #change this when user index is working
+            redirect_to root_path
         else
+            #error message on fail
             message = "Something went wrong! Make sure your username and password are correct and try again."
             redirect_to login_path, notice: message
         end
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_06_26_013150) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
